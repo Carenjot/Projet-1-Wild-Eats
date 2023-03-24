02643fe12e56ceb87ced761f76d56dcc5ae0c323
Nassim Cheikh rouhou <nassimcheikhrouhou@gmail.com>
2023-03-24 10:46:27 +0100

    ajout de cartes

--- cards.js
+++ cards.js
@@ -24,7 +24,7 @@ const restoDispo = [
     adresse: "63 bis, rue de Chativesle",
     link: "https://www.restaurant-lalambic.fr/",
     image: "img/lalambic.png",
-    categorie: "français"
+    categorie: "français",
   },
   {
     restaurant: "Green sur mesure",
@@ -32,7 +32,7 @@ const restoDispo = [
     adresse: "4 Bis rue Gaston Boyer",
     link: "https://www.greensurmesure.fr/le-concept/",
     image: "img/greensurmesure.png",
-    categorie: "veggie"
+    categorie: "veggie",
   },
   {
     restaurant: "Koboon",
@@ -40,7 +40,7 @@ const restoDispo = [
     adresse: "9, boulevard du Général Leclerc",
     link: "https://www.koboon-resto.fr/",
     image: "img/koboon.png",
-    categorie: "veggie"
+    categorie: "veggie",
   },
   {
     restaurant: "Come Prima",
@@ -48,7 +48,7 @@ const restoDispo = [
     adresse: "3 rue Buirette",
     link: "https://come-prima-reims.eatbu.com/?lang=fr",
     image: "img/comeprima.png",
-    categorie: "italien"
+    categorie: "italien",
   },
   {
     restaurant: "L’Oriental",
@@ -56,7 +56,39 @@ const restoDispo = [
     adresse: "13 rue Gerbert",
     link: "http://restaurant-couscous-reims.com/",
     image: "img/loriental.png",
-    categorie: "other"
+    categorie: "other",
+  },
+  {
+    restaurant: "Mizzica",
+    desc: "Produits siciliens et spécialités italiennes à Reims : l'épicerie Mizzica vous propose des produits artisanaux.",
+    adresse: "60 Rue Gambetta",
+    link: "https://www.mizzicareims.fr/",
+    image: "img/Mizzicacard.png",
+    categorie: "italien",
+  },
+  {
+    restaurant: "Marcel & Jane",
+    desc: "Faites-vous plaisir avec les délicieuses recettes de Marcel & Jane, une cuisine bio et raisonnée",
+    adresse: "99 Rue Gambetta",
+    link: "https://www.facebook.com/MarcelandJane",
+    image: "img/M&Jcard.png",
+    categorie: "veggie",
+  },
+  {
+    restaurant: "Zwiebel",
+    desc: "le 1er K E B A P 100% BERLINOIS de Reims",
+    adresse: "103 Rue de Neufchâtel",
+    link: "https://zwiebel-reims.business.site/",
+    image: "img/Zwiebielcard.png",
+    categorie: "other",
+  },
+  {
+    restaurant: "Le Parc Les Crayères",
+    desc: "Découvrez le restaurant Le Parc et notre chef étoilé au Domaine Les Crayères",
+    adresse: "64 Boulevard Henry Vasnier",
+    link: "https://lescrayeres.com/fr/restaurant-le-parc.html",
+    image: "img/C.png",
+    categorie: "français",
   },
 ];
 //Create cards
@@ -75,15 +107,19 @@ function CreateCard(restaurant) {
   `;
 }
 //Index.js
-function createAllCards(filter , categorie) {
+function createAllCards(filter, categorie) {
   let cards = "";
   for (let i = 0; i < restoDispo.length; i++) {
-    if ((filter == null || restoDispo[i].restaurant.startsWith(filter)) || categorie == restoDispo[i].categorie) {
+    if (
+      filter == null ||
+      restoDispo[i].restaurant.startsWith(filter) ||
+      categorie == restoDispo[i].categorie
+    ) {
       cards += CreateCard(restoDispo[i]);
     }
   }
 
-  if(cards == ""){
+  if (cards == "") {
     cards = `<h2 class = "no-result" >Pas de résultats</h2>`;
   }
   return cards;
@@ -96,15 +132,16 @@ document.querySelector(".search-bar").addEventListener("change", () => {
 
 document.querySelector(".list-resto").innerHTML = createAllCards();
 
-
 let buttons = document.getElementsByTagName("div");
 
-for(let i = 0 ; i < buttons.length ; i++){
-  if(buttons[i].className == "button-categorie"){
-    buttons[i].addEventListener("click", () =>{
-      const categorie = buttons[i].getAttribute('id');
-      document.querySelector(".list-resto").innerHTML = createAllCards("no filter", categorie);  
+for (let i = 0; i < buttons.length; i++) {
+  if (buttons[i].className == "button-categorie") {
+    buttons[i].addEventListener("click", () => {
+      const categorie = buttons[i].getAttribute("id");
+      document.querySelector(".list-resto").innerHTML = createAllCards(
+        "no filter",
+        categorie
+      );
     });
   }
 }
-
